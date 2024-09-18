4c036235f446a9be0a3a1eb45d3251eabeccf805
zakahfir <ahfirzakaria28@gmail.com>
2024-09-18 15:13:53 +0200

    Update main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,4 +1,22 @@
-# Ecrivez vos fonctions ici
+def salaire_mensuel(salaire_annuel):
+    return salaire_annuel / 12
+
+def salaire_hebdomadaire(salaire_mensuel):
+    return salaire_mensuel / 4
+
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees)
+    return salaire_hebdomadaire / heures_travaillees
+
+def main():
+    salaire_annuel = float(input("Entre votre salaire annuel : "))
+    heures_travaillees = float(input("Entre votre nombre d'heures travaillées par semaine : "))
+
+mensuel = salaire_mensuel(salaire_annuel)
+hebdomadaire = salaire_hebdomadaire(salaire_mensuel):
+horaire = salaire_horaire(salaire_hebdomadaire, heures_travaillees)
+
+print("Votre salaire est de", horaire, "euros.")
+
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
