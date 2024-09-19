8c21836f95ccaf17eb6d616492babb5566e72a8e
zakahfir <ahfirzakaria28@gmail.com>
2024-09-19 19:02:18 +0200

    main.py

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,4 +1,10 @@
-# Ecrivez votre code ici !
+import operations
+
+def main():
+    resultat_addition = operations.addition(3, 5)
+    print("Le resultat de l'addition est", resultat_addition)
+    resultat_multiplication = operatiions.multiplication(8, 2)
+    print("Le resultat de la multiplication est", resultat_multiplication)
 
 
 # Ne touchez pas le code ci-dessous
